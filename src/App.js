9c0348608ec49a0436ef021c0e8bf1d879ea8ed0
Arseniy Voynov (Arsenio Draco) <voynovarseny@gmail.com>
2026-02-07 21:59:50 +0300

    Add Watch component to display current time

--- src/App.js
+++ src/App.js
@@ -1,1 +1,14 @@
+import React, { useEffect, useState } from 'react';
 
+const Watch = () => {
+  const [time, setTime] = useState(new Date());
+
+  useEffect(() => {
+    const timer = setInterval(() => {
+      setTime(new Date());
+    }, 1000);
+    return () => clearInterval(timer);
+  }, []);
+
+
+  
