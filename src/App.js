f107d94f2d2512a853bb351c1aab73993b81ca1b
Arseniy Voynov (Arsenio Draco) <voynovarseny@gmail.com>
2026-02-08 18:01:39 +0300

    Add UTC time calculations for clock display

--- src/App.js
+++ src/App.js
@@ -10,5 +10,13 @@ const Watch = () => {
     return () => clearInterval(timer);
   }, []);
 
+  const utcHours = time.getUTCHours();
+  const utcMinutes = time.getUTCMinutes();
+  const utcSeconds = time.getUTCSeconds();
+
+  const hourDeg = (utcHours % 12) * 30 + utcMinutes * 0.5;
+  const minuteDeg = utcMinutes * 6 + utcSeconds * 0.1;
+  const secondDeg = utcSeconds * 6;
+  
 
   
